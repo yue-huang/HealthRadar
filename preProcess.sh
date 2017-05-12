dbc21ca53b815a3f433a2badb03cb8676e2a01da
Yue Huang <yue.daphne.huang@gmail.com>
2017-05-11 21:55:19 -0500

    Remove v0.1 code

--- preProcess.sh
+++
@@ -1,7 +0,0 @@
-python3 stateCancer.py LLCP2015.txt 112 > stateCancer15.csv
-python3 stateCancer.py LLCP2014.txt 100 > stateCancer14.csv
-python3 stateCancer.py LLCP2013.txt 104 > stateCancer13.csv
-python3 stateCancer.py LLCP2012.txt 91 > stateCancer12.csv
-python3 stateCancer.py LLCP2011.txt 95 > stateCancer11.csv
-
-python3 cancerSubset.py LLCP2015.txt 112 > cancerSubset15.csv
